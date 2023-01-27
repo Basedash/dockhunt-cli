9df76da166fe34ae5a51626156845df8c98dff59
Jeremy Sarchet <jeremy@jeremysarchet.com>
2023-01-27 12:27:42 +0100

    Initial parsing of the dock plist XML

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var xml2js = require('xml2js');
+const util = require('util')
+
 function getDockContents(dockXmlPlist) {
   console.log('Your dock contents:')
 
-  console.log(dockXmlPlist);
+  xml2js.parseStringPromise(dockXmlPlist).then(function (dockData) {
+    // https://stackoverflow.com/a/10729284/15487978
+    console.log(util.inspect(dockData, { depth: null, colors: true }));
+    console.log('Done');
+  })
+  .catch(function (err) {
+    console.error('Problem parsing XML plist');
+  });
 }
 
 module.exports = getDockContents;
