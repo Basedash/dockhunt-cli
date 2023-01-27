531f43fe86cf1d966f9116719a1124249c62f3d0
Jeremy Sarchet <jeremy@jeremysarchet.com>
2023-01-27 14:18:35 +0100

    Extract app names and directories from the XML

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 var xml2js = require('xml2js');
 const util = require('util')
 
-function getDockContents(dockXmlPlist) {
-  console.log('Your dock contents:')
+function logWithInspect(object) {
+  // https://stackoverflow.com/a/10729284/15487978
+  console.log(util.inspect(object, { depth: null, colors: true }));
+}
 
-  xml2js.parseStringPromise(dockXmlPlist).then(function (dockData) {
-    // https://stackoverflow.com/a/10729284/15487978
-    console.log(util.inspect(dockData, { depth: null, colors: true }));
-    console.log('Done');
+function getDockContents(dockXmlPlist) {
+  xml2js.parseStringPromise(dockXmlPlist).then(function (rawDockData) {
+    // TODO: Ensure XML is valid apple p-list
+    const rawAppData = rawDockData.plist.dict[0]
+
+    //logWithInspect(appData);
+
+    const persistentApps = rawAppData.array[1].dict;
+    const _persistentOthers = rawAppData.array[2].dict;
+    const _recentApps = rawAppData.array[3].dict;
+
+    const appData = [];
+
+    for (const rawAppData of persistentApps) {
+      const appName = rawAppData.dict[0].string[1];
+      const appDirectory = rawAppData.dict[0].dict[0].string[0];
+      appData.push({
+        appName,
+        appDirectory,
+      });
+    }
+
+    console.log('Found the following persistent apps:')
+    logWithInspect(appData);
+
   })
-  .catch(function (err) {
-    console.error('Problem parsing XML plist');
+  .catch(function (error) {
+    console.error('Problem parsing XML p-list');
+    console.error(error);
   });
 }
 
